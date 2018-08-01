309d258083805f963f6b7633739cf5a8068da67b
Raheel <s.raheelshaikh@gmail.com>
2018-08-01 19:06:09 +0500

    commit

--- modules/sales/print.php
+++ modules/sales/print.php
@@ -66,24 +66,23 @@ table th, table td {
 	text-align: left;
 }
 table th {
-    border: 1px solid #fff;
-    color: #fff;
+    border: 1px solid #000;
+    color: #000;
     font-weight: bold;
     line-height: 0.9em;
-    padding: 10px 5px;
+    padding: 5px 5px;
     text-align: center;
-	background-color:#000;
     white-space: nowrap;
 	font-size: 1.4em;
 }
-.contentbox p{ font-size:1.1em}
+.contentbox p{ font-size:0.9em}
 table td {
 	text-align: right;
-	padding-top: 6px;
-	padding-right: 6px;
-	padding-bottom: 6px;
-	padding-left: 6px;
-	font-size: 1.4em;
+	padding-top: 4px;
+	padding-right: 4px;
+	padding-bottom: 4px;
+	padding-left: 4px;
+	font-size: 1.2em;
 }
 table tr{ font-size:10px}
 table td.service, table td.desc {
@@ -98,9 +97,9 @@ table td.grand {
 }
 
 #signcompny {
-    border-top: thin solid #000;
-    margin: 10px 0 0;
-    padding-top: 10px;
+    border-top: thin solid #999;
+    margin: 5px 0 0;
+	padding-top: 5px;
     text-align: center;
 }
 footer {
@@ -125,16 +124,16 @@ footer {
 	font-size:1.7em;
 	text-transform:uppercase;
 }
-.address{ text-align:center;font-size: 1.2em;padding: 0 35px;}
+.address{ text-align:center;font-size: 1em;padding: 0 35px;}
 #order {
     border: 1px solid #000000;
     border-radius: 5px;
     color: #000000;
     display: block;
-    font-size: 1.2em;
+    font-size: 1.5em;
     font-weight: bold;
     line-height: 16px;
-    margin: 10px auto 10px;
+    margin: 5px auto 5px;
     padding: 5px;
     text-align: center;
     width: 180px;
@@ -150,10 +149,10 @@ footer {
 	padding: 10px 5px;
 	border-radius: 100%;
 	display: inline-block;
-	width: 38px;
-	height: 28px;
+	width: 30px;
+	height: 18px;
 	text-align: center;
-	line-height: 28px;
+	line-height: 20px;
 }
 .barcode img{ width:145px;}
 .item_name {
@@ -207,14 +206,15 @@ footer {
 	$total_discount = 0;
 	?>
 	<div class="left-col">
-    	<div id="logo">
+    	<!--<div id="logo">
     		<?php $reciept_logo=get_config("reciept_logo"); if(empty($reciept_logo)) echo $site_title; else { ?><img src="<?php echo $file_upload_root;?>config/<?php echo $reciept_logo?>" /><?php }?>
-    	</div>
-    	<span class="address"><?php echo nl2br(get_config("address_phone"))?></span>
+    	</div>-->
+    	<!--<span class="address"><?php echo nl2br(get_config("address_phone"))?></span>-->
     	<div id="order">Order ID: <strong><?php echo $order_id; ?></strong></div>
         <div class="contentbox">
-            <p>Date/Time: <strong style="float:right"><?php echo datetime_convert($sale["datetime_added"]); ?></strong></p>
-            <p>Customer: <strong style="float:right"><?php echo get_field($sale["account_id"], "account","title"); ?></strong></p>
+            <p style="float:left; margin-top:10px">Date/Time:<br> <strong><?php echo datetime_convert($sale["datetime_added"]); ?></strong></p>
+            <p style="float:right; text-align:right; font-size:1.3em">Customer:<br> <strong><?php echo get_field($sale["account_id"], "account","title"); ?></strong> <?php if(empty($sale["transaction_id"])){ echo "(CREDIT)";}?></p>
+            <!--<p style="float:right; font-size:1.3em"><span><?php if(empty($sale["transaction_id"])){ echo "(CREDIT)";}?></span></p>-->
             <table cellpadding="0" cellspacing="0" align="center" width="800" border="0" class="">
                 <tr>
                     <th width="7%">S#</th>
@@ -293,6 +293,14 @@ footer {
             }
             ?>
         </table>
+        <hr style="border:0; border-top:1px solid #999; margin:0 0 5px;">
+        <p><strong>TOTAL</strong><strong style="float:right"> <?php 
+            $total_price= dofetch(doquery("select sum(total_price) as total_price from sales_items where sales_id='".$sale["id"]."'", $dblink));
+            echo curr_format($total_price["total_price"]);
+        
+        ?></strong></p>
+        <p><strong>Discount</strong><strong style="float:right"> <?php echo curr_format($sale["discount"])?></strong></p>
+        <p><strong>TOTAL</strong><strong style="float:right"> <?php echo curr_format($total_price["total_price"] -$sale["discount"])?></strong></p>
     </div>
     <div id="signcompny">Software developed by wamtSol http://wamtsol.com/ - 0346 3891 662</div>
     </div> 
